b84da8366b7fdbb416a693758690de3c40ff4d77
Kelly Anlas <kmma31@gmail.com>
2020-07-14 21:14:21 -0400

    WaPo + email link
    1) Added WaPo to matches
    2) Added text and button to 'get in touch' which links to email with default text

--- popup.js
+++ popup.js
@@ -9,3 +9,12 @@ function setAction() {
     chrome.tabs.sendMessage(tabs[0].id, bool);
   });
 }
+
+let email = document.getElementById('email')
+email.addEventListener('click', sendEmail)
+
+let mailto = "mailto:politinudge@gmail.com?subject=I%20want%20to%20be%20on%20PolitiNudge!&body=Tell%20us%20about%20your%20organization/event,%20and%20remember%20to%20attach%20your%20image!";
+
+function sendEmail() {
+	window.open(mailto, 'popUpWindow');
+}
