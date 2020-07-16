289a969b5348b4a87655d5f09d1bb1e55cffbddb
M S <monicashiwratan@gmail.com>
2020-07-16 18:26:46 -0400

    Now updating <a> tag href for each image with corresponding url

--- content.js
+++ content.js
@@ -5,18 +5,6 @@ function changeImages()
 	changeImages(true);
 }
 
-// function changeImages(puppies){
-//     const imgTagCount = document.getElementsByTagName('img').length;
-//     const imgTags = document.getElementsByTagName('img');
-
-//     let i;
-//     for (i = 0; i < imgTagCount; i++) {
-//         console.log('OLD img src: ', imgTags[i].src);
-        
-//         let newImage;
-//         if (puppies) newImage = chrome.extension.getURL('images/puppy.png');
-// 	    else newImage = chrome.extension.getURL('images/img1.jpg'); 
-
 function changeImages(puppies){
     const imgTagCount = document.getElementsByTagName('img').length;
     const imgTags = document.getElementsByTagName('img');
@@ -28,35 +16,46 @@ function changeImages(puppies){
         let pickImage = evaluateImage(oldWidth, oldheight);
         let newImage;
         
-        if (puppies) newImage = chrome.extension.getURL('images/puppy.png');
-   	    else newImage = chrome.extension.getURL(pickImage);
+        newImage = chrome.extension.getURL(pickImage.img);
+        newUrl = chrome.extension.getURL(pickImage.url);
 
         imgTags[i].src = newImage;
-        // imgTags[i].style.width = 'auto';
-        // imgTags[i].style.height = 'auto';
-        imgTags[i].width = oldWidth;
-        imgTags[i].height = oldheight;
+        imgTags[i].closest('a').href = newUrl.slice(52);
+        imgTags[i].style.width = '100%';
+        imgTags[i].style.height = '100%';
     }
 }
 
 function evaluateImage(width, height){
-    let moreImages = [ 
-        'images/BLM.jpg',
-        'images/Ballet.jpg',
-        'images/March_on_Washington.jpg',
-        'images/She_Sould_Run.jpg',
-        'images/US_of_Young Women.jpg',
-        'images/Vote_Save_America.jpg' 
-        ];
+    const a = "https://blacklivesmatter.com";
+    const b = "https://www.vote411.org";
+    const c = "https://nationalactionnetwork.net/events";
+    const d = "https://www.sheshouldrun.org";
+    const e = "https://www.theunitedstateofwomen.org/youth";
+    const f = "https://votesaveamerica.com";
+    const g = "https://www.voterunlead.org";
+
+    let moreImages = [
+        {img:"images/BLM.jpg", url:a},
+        {img:"images/Ballet.jpg", url:b},
+        {img:"images/March_on_Washington.jpg", url:c},
+        {img:"images/She_Sould_Run.jpg", url:d},
+        {img:"images/US_of_Young_Women.jpg", url:e},
+        {img:"images/Vote_Save_America.jpg", url:f} 
+    ];
 
     if (height < 0.6*width){
-        return 'images/Vote_Run_Lead.jpg';
-    } else if (width < 0.6*height){
-        return 'images/Vote_411.jpg';
-    } else {
+        return {img:"images/Vote_Run_Lead.jpg", url:g};
+    } 
+    
+    else if (width < 0.6*height){
+        return {img:"images/Vote_411.jpg", url:b};
+    } 
+    
+    else {
         let n = Math.floor(Math.random() * moreImages.length);
         let randomImage = moreImages[n]; 
         return randomImage;
-    }
+  }
 
 }
